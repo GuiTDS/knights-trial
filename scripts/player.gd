e52ceaa331684748cffe13d2da1a9e8132d75936
Guilherme Sanches <guitdsanches@gmail.com>
2025-10-23 11:55:42 -0300

    :sparkles: feat(platform): add moving platform and fixing world decoration

--- scripts/player.gd
+++ scripts/player.gd
@@ -1,6 +1,5 @@
 extends CharacterBody2D
 
-
 const SPEED = 200.0
 const JUMP_VELOCITY = -400.0
 
@@ -14,13 +13,13 @@ func _physics_process(delta: float) -> void:
 		velocity += get_gravity() * delta
 
 	# Handle jump.
-	if Input.is_action_just_pressed("ui_accept") and is_on_floor():
+	if Input.is_action_just_pressed("move_up") and is_on_floor():
 		velocity.y = JUMP_VELOCITY
 		is_jumping = true
 	elif is_on_floor():
 		is_jumping = false
 
-	var direction := Input.get_axis("ui_left", "ui_right")
+	var direction := Input.get_axis("move_left", "move_right")
 	if direction:
 		velocity.x = direction * SPEED
 		animation.scale.x = direction # change player direction

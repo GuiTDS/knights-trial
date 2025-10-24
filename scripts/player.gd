bf527a8296924bd9bd63af4e293e23be1b5852bd
Guilherme Sanches <guitdsanches@gmail.com>
2025-10-24 09:04:12 -0300

    :sparkles: feat(player): mini state machine, die and hurt animations

--- scripts/player.gd
+++ scripts/player.gd
@@ -2,10 +2,19 @@ extends CharacterBody2D
 
 const SPEED = 200.0
 const JUMP_VELOCITY = -400.0
-
-var is_jumping := false
+const X_COLLIDING_FORCE = 200
 
 @onready var animation := $anim as AnimatedSprite2D
+@onready var remote_transform := $remote as RemoteTransform2D
+@onready var ray_right := $ray_right as RayCast2D
+@onready var ray_left := $ray_left as RayCast2D
+
+var is_jumping := false
+var is_hurted := false
+var is_dead := false
+var player_life := 4 # 5 hearts
+var knockback_vector := Vector2.ZERO
+var direction
 
 func _physics_process(delta: float) -> void:
 	# Add the gravity.
@@ -18,17 +27,61 @@ func _physics_process(delta: float) -> void:
 		is_jumping = true
 	elif is_on_floor():
 		is_jumping = false
-
-	var direction := Input.get_axis("move_left", "move_right")
+		
+	direction = Input.get_axis("move_left", "move_right")
 	if direction:
 		velocity.x = direction * SPEED
 		animation.scale.x = direction # change player direction
-		if !is_jumping:
-			animation.play("run")
-	elif is_jumping:
-		animation.play("jump")
 	else:
 		velocity.x = move_toward(velocity.x, 0, SPEED)
-		animation.play("idle")
-
+	
+	if knockback_vector != Vector2.ZERO:
+		velocity = knockback_vector
+	_set_state()
 	move_and_slide()
+
+func follow_camera(camera): 
+	var camera_path = camera.get_path()
+	remote_transform.remote_path = camera_path
+
+func _on_hurtbox_body_entered(_body: Node2D) -> void:
+	if player_life == 0:
+		is_dead = true
+		await get_tree().create_timer(1).timeout
+		queue_free()
+	else:
+		if ray_right.is_colliding():
+			take_damage(Vector2(-X_COLLIDING_FORCE, -200))
+		elif ray_left.is_colliding():
+			take_damage(Vector2(X_COLLIDING_FORCE, -200))
+		
+func take_damage(knockback_force := Vector2.ZERO, duration := 0.25):
+	player_life -= 1
+
+	if knockback_force != Vector2.ZERO:
+		knockback_vector = knockback_force
+		
+		var knockback_tween = get_tree().create_tween()
+		knockback_tween.parallel().tween_property(self, "knockback_vector", Vector2.ZERO, duration)
+		animation.modulate = Color(1, 0, 0, 1)
+		knockback_tween.parallel().tween_property(animation, "modulate", Color(1, 1, 1, 1), duration)
+	is_hurted = true
+	await get_tree().create_timer(.3).timeout
+	is_hurted = false
+	
+func _set_state():
+	var state = "idle"
+	
+	if !is_on_floor():
+		state = "jump"
+	elif direction != 0:
+		state = "run"
+	
+	if is_hurted:
+		state = "hurt"
+		
+	if is_dead:
+		state = "die"
+		
+	if animation.name != state:
+		animation.play(state)
--- scripts/skeleton.gd
+++ scripts/skeleton.gd
@@ -7,21 +7,35 @@ const JUMP_VELOCITY = -400.0
 @onready var anim := $anim as AnimatedSprite2D
 
 var direction := -1
+var is_dying = false
 
 func _physics_process(delta: float) -> void:
 	# Add the gravity.
 	if not is_on_floor():
 		velocity += get_gravity() * delta
+	
+	if is_dying:
+		velocity = Vector2.ZERO
+		move_and_slide()
+		return
 		
 	if collision_detector.is_colliding():
 		direction *= -1
 		collision_detector.scale.x *= -1
 	
-	if direction == -1:
-		anim.flip_h = true
-	else:
-		anim.flip_h = false
+	anim.flip_h = direction == -1 # flip sprite
 		
 	velocity.x = direction * SPEED * delta
 
 	move_and_slide()
+
+func _on_anim_animation_finished() -> void:
+	if anim.animation == "die":
+		queue_free()
+
+
+func _on_anim_animation_changed() -> void:
+	if anim == null:
+		return
+	if anim.animation == "die":
+		is_dying = true
